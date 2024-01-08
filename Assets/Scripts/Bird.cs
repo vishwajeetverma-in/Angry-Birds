6fbaa4715a3cd6037294aa3587f826ea97dce23c
vishwajeetverma-in <vishwa11126@gmail.com>
2024-01-08 17:56:23 +0530

    height and range of projectile was very low after adding gravity to1 | so vector parameter passed inside force should be multiplied with a larger float value of order 500 to get the desired effect | also in update() reload the scene if bird is out of scene window (hardcode the scene by observing y coordinate of bird)

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -1,14 +1,26 @@
 using System.Collections;          // list of objects
 using System.Collections.Generic; // list of birds(class name)
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Bird : MonoBehaviour
 {
     Vector3 _initialPosition;
+    private float _launchPower = 500;
 
     private void Awake()
     {
         _initialPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        if (transform.position.y > 10f)
+        {
+            string currentSceneName = SceneManager.GetActiveScene().name;
+            SceneManager.LoadScene(currentSceneName);
+            
+        }
     }
     private void OnMouseDown()
     {
@@ -20,7 +32,7 @@ public class Bird : MonoBehaviour
     {
         GetComponent <SpriteRenderer>().color = Color.green;
         Vector2 directionToInitialPosition = _initialPosition - transform.position;
-        GetComponent<Rigidbody2D>().AddForce(directionToInitialPosition * 100);
+        GetComponent<Rigidbody2D>().AddForce(directionToInitialPosition * _launchPower);
         GetComponent<Rigidbody2D>().gravityScale = 1;
     }
 
