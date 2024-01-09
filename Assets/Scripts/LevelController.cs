bc94cdd58024fb6e2de3cbb1207aa3308eef4811
vishwajeetverma-in <vishwa11126@gmail.com>
2024-01-09 19:05:54 +0530

    levelController set | 3 levels

---
+++ Assets/Scripts/LevelController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelController : MonoBehaviour
+{
+    private static int _nextLevelIndex = 1;
+    private Enemy[] _enemies;
+
+    private void OnEnable()
+    {
+        _enemies = FindObjectsOfType<Enemy>();
+        
+    }
+    private void Update()
+    {
+        foreach (Enemy enemy in _enemies)
+        {
+            if (enemy != null)
+            {
+                return;
+            }
+        }
+
+        _nextLevelIndex++;
+        string nextLevelName = "Level" + _nextLevelIndex;
+        SceneManager.LoadScene(nextLevelName);
+    }
+
+}
